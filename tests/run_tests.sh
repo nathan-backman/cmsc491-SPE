f7fada53c4912870978f5a40f4481db226bdd32a
Nathan Backman <backman@bvu.edu>
2019-09-09 03:43:08 -0500

    Added a script that will run all well-structured tests that live within the tests folder. It requires that they're properly built first which we can vet through a Makefile later and then only invoke this script through that makefile.

---
+++ tests/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+successes=$((0))
+failures=$((0))
+
+for testDir in */; do
+  testName="${testDir%?}"
+  echo -n "Running test [$testName]: "
+
+  cd $testName
+
+  ./$testName.app < input.data > test.output
+  diff output.data test.output > /dev/null
+  if [ "$?" -ne "0" ] ; then
+    echo -e "\e[1;31mFailed\e[0m"
+    failures=$((failures+1))
+  else
+    echo -e "\e[1;32mPassed\e[0m"
+    successes=$((successes+1))
+  fi
+  rm -f test.output
+
+  cd ..
+done
+
+echo "--------------"
+echo "Finished Tests"
+echo -e "  \e[1;32mPassed $successes\e[0m"
+echo -e "  \e[1;31mFailed $failures\e[0m"
+
