b4a98596f44b8c4a21ed3020236e4820f77d05a4
Nathan Backman <backman@bvu.edu>
2019-09-21 14:09:57 -0500

    Added another test from hw_2

---
+++ tests/IncrementOp_test/driver.cpp
@@ -0,0 +1,43 @@
+// Copyright 2019 [BVU CMSC491 class]
+#include <iostream>
+#include <string>
+#include <vector>
+#include "SPE.h"
+
+class FileReader : public InputSource {
+  void generateData() {
+    std::string line;
+    while (getline(std::cin, line)) {
+      emit(Data(line));
+    }
+  }
+};
+
+class IncrementOp : public Operator {
+ public:
+  void processData(Data data) {
+    int num = std::stoi(data.value);
+    num++;
+    num *= 10;
+    data.value = std::to_string(num);
+    emit(data);
+  }
+};
+
+class PrintData : public Operator {
+ public:
+  void processData(Data data) { std::cout << data.value << std::endl; }
+};
+
+int main(int argc, char** argv) {
+  FileReader inputSource;
+  IncrementOp op1;
+  PrintData op2;
+
+  StreamProcessingEngine spe;
+  spe.addInputSource(&inputSource, {&op1});
+  spe.connectOperators(&op1, {&op2});
+  spe.run();
+
+  return 0;
+}
